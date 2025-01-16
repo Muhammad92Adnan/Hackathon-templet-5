66564e61e485fc32e2087aa441fc61ba363db59d
Muhammad_Adnan <adnan92muhammad@gmail.com>
2025-01-16 11:11:20 +0500

    Hackathon-2-Template-5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Montserrat } from 'next/font/google';
@@ -10,16 +10,16 @@ const montserrat = Montserrat({
   weight: ['400', '700'], 
 });
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "BANDAGE",
